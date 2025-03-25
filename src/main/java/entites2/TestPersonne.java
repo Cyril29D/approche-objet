f526611a3024ed0409bd63448bb55603c0ca0b79
Cyril29D <cyrildelly9@gmail.com>
2025-03-25 14:20:59 +0100

    tp3

--- src/main/java/entites2/TestPersonne.java
+++ src/main/java/entites2/TestPersonne.java
@@ -9,5 +9,10 @@ public class TestPersonne {
         p2.adr3.numeroRue = 22 ;
         p2.adr3.codePostal = 34000;
         p2.adr3.libelleRue = "test";
+
+        System.out.println("Nom : " + p2.getNom());
+        System.out.println("Prénom : " + p2.getPrenom());
+        System.out.println("Adresse : " + p2.getAdr3().getNumeroRue() + " " + p2.getAdr3().getLibelleRue() + ", "
+                + p2.getAdr3().getCodePostal() + " " + p2.getAdr3().getVille());
     }
 }
--- src/main/java/entites2/personne.java
+++ src/main/java/entites2/personne.java
@@ -17,4 +17,30 @@ public class personne {
       this.prenom = prenom;
       this.adr3 = adr3;
    }
+
+   public void afficherNomMaj() {
+      System.out.println(prenom + " " + nom.toUpperCase());
+   }
+   public void modifierNom(String nouveauNom) {
+      this.nom = nouveauNom;
+   }
+   public void modifierPrenom(String nouveauPrenom){
+      this.prenom = nouveauPrenom ;
+   }
+   public void modifierAdresse(AdressePostale adr4){
+      this.adr3 = adr4 ;
+   }
+
+   public String getNom() {
+      return nom;
+   }
+
+   public String getPrenom() {
+      return prenom;
+   }
+
+   public AdressePostale getAdr3() {
+      return adr3;
+   }
+
 }

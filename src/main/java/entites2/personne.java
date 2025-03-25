c5de9c42edec5c8f34d17ee560c58db45b55ba0c
Cyril29D <cyrildelly9@gmail.com>
2025-03-25 11:19:01 +0100

    tp1

---
+++ src/main/java/entites2/personne.java
@@ -0,0 +1,9 @@
+package entites2;
+
+import entites.AdressePostale;
+
+public class personne {
+   public String nom ;
+   public String prenom ;
+   public AdressePostale adr3 = new AdressePostale();
+}
